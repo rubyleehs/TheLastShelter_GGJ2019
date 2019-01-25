94442fe40c728541ca4a0766c5d32392e5aee492
rubyleehs <rubyleehs@live.com>
2019-01-25 22:53:08 +0800

    Live Entity

---
+++ Assets/Scripts/Ruby/LiveEntity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class LiveEntity : MonoBehaviour
+{
+
+    //Stats
+    public float maxHP;
+    public float currentHP;
+    public float speed;
+
+    //Runtime Values
+    public float lookAngle;
+
+    //Refrences
+    public new Transform transform;
+    public Rigidbody2D rb;
+
+    public virtual void Awake()
+    {
+        transform = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public abstract void Move(Vector2 direction);
+    public abstract void Attack();
+
+    public virtual void TakeDamage(float amount)
+    {
+        currentHP -= amount;
+
+        if (currentHP <= 0) Die();
+        if (currentHP > maxHP) currentHP = maxHP;
+    }
+
+    public abstract void Die();
+}
