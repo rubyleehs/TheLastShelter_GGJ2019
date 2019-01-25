dd2d32183561d73cd44883a049816fc1d9c95823
lchkonoka <felixlch96@gmail.com>
2019-01-26 00:06:59 +0800

    enemy pathfind

--- Assets/Scripts/Ruby/GameManager.cs
+++ Assets/Scripts/Ruby/GameManager.cs
@@ -7,9 +7,19 @@ public class GameManager : MonoBehaviour {
     public static float deltaTime;
     protected float timeScale = 1;
 
-    public List<Transform> friendlies;
+    public static List<Transform> friendlies = new List<Transform>();
 
-	void Update ()
+    private void Start()
+    {
+        //adding all friendly object's transform into list for pathfinding reference
+        GameObject[] friendlyObj = GameObject.FindGameObjectsWithTag("FRIENDLIES");
+        print(friendlyObj.Length);
+        for (int x = 0; x < friendlyObj.Length; x++)
+            friendlies.Add(friendlyObj[x].transform);
+
+        print(friendlies.Count);
+    }
+    void Update ()
     {
         deltaTime = Time.deltaTime * timeScale;
 	}
