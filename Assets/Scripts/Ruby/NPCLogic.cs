d3f13cdfc5890e1ea6f836103b9cf0ef6216370c
rubyleehs <rubyleehs@live.com>
2019-01-26 22:08:18 +0800

    NPC prep

---
+++ Assets/Scripts/Ruby/NPCLogic.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCLogic : LiveEntity
+{
+    [Header("Wander Logic")]
+    public Vector2 homePos;
+    public Vector2 wanderRadius;
+    public Vector2 stepDistance;
+    public float wanderCheckIntervalDuration;
+    public Transform wanderFollow;
+
+    [Range(0,1)]
+    public float wanderChance;
+
+    private Vector2 targetPos;
+    private Vector2 delta;
+    private float timeSinceLastWanderCheck;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        FindRandomTargetPos();
+    }
+
+    private void Update()
+    {
+        SmartMove();
+    }
+
+    private void SmartMove()
+    {
+        delta = (Vector2)transform.position - targetPos;
+        timeSinceLastWanderCheck += GameManager.deltaTime;
+        if (delta.sqrMagnitude < 1 && timeSinceLastWanderCheck > wanderCheckIntervalDuration) 
+        {
+            timeSinceLastWanderCheck = 0;
+            if (Random.Range(0f, 1f) <= wanderChance) FindRandomTargetPos();
+        }
+
+        animator.SetBool("isMoving", rb.velocity.sqrMagnitude > 0.2f);
+        animator.SetInteger("YFaceDir", (int)(cardinalLookAngle / 90) % 2);
+
+        Move(delta);
+        Face(delta,true);
+    }
+
+    private void FindRandomTargetPos()
+    {
+        if (wanderFollow == null) wanderFollow = transform;
+        bool isValidTargetPos = false;
+        float d = 0;
+        while (!isValidTargetPos)
+        {
+            targetPos = (Vector2)wanderFollow.position + Random.insideUnitCircle * Random.Range(stepDistance.x, stepDistance.y);
+            d = (homePos - targetPos).magnitude;
+            if (d > wanderRadius.x && d < wanderRadius.y) isValidTargetPos.Equals(true);
+        }
+    }
+    
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
